7d67a2534e8625e1de2689f3c44c994b747bd478
dohnnyj3pp <dohnnypuppet@gmail.com>
2025-01-11 17:25:14 -0700

    Added some counting loop practice.

---
+++ ConsoleAppAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a do while loop
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Inside the loop");
+                count++; //add one to count each time
+            }
+            while (count < 5); //continue to loop until count reaches 5
+
+            //This is a while loop
+            int counting = 0;
+            while (counting < 3) //Continue to loop until counting reaches 3
+            {
+                Console.WriteLine("Counting number");
+                counting++; //add one to counting each time
+            }
+            Console.ReadLine();
+        }
+    }
+}
