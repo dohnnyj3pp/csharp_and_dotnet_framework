3cc7178bdc596cfbe2181e415bd4642fcda58abc
dohnnyj3pp <dohnnypuppet@gmail.com>
2025-01-14 13:24:22 -0700

    added another Math Operations App Assignment for practice

---
+++ MathOperationsApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MathOperationsApp
+{
+    // Define a class called MathOperations
+    class MathOperations
+    {
+        // Method that takes two integers as parameters, with the second one being optional
+        public int Add(int firstNumber, int secondNumber = 0)
+        {
+            // Perform addition and return the result
+            return firstNumber + secondNumber;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Prompt the user for the first number
+            Console.WriteLine("Please enter the first number:");
+            // Read and parse the input as an integer
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            // Prompt the user for the second number, indicating it's optional
+            Console.WriteLine("Please enter the second number (press Enter to skip):");
+            string input = Console.ReadLine();
+            int secondNumber;
+
+            // Check if the user entered a second number
+            if (string.IsNullOrEmpty(input))
+            {
+                // If not, call the Add method with only the first number
+                int result = mathOps.Add(firstNumber);
+                Console.WriteLine($"The result of adding {firstNumber} and the default (0) is: {result}");
+            }
+            else
+            {
+                // Parse the second number if provided
+                secondNumber = int.Parse(input);
+                // Call the Add method with both numbers
+                int result = mathOps.Add(firstNumber, secondNumber);
+                Console.WriteLine($"The result of adding {firstNumber} and {secondNumber} is: {result}");
+            }
+
+            // Optional: Test various combinations of numbers
+            Console.WriteLine("\nTesting various combinations:");
+            Console.WriteLine($"Add(5) = {mathOps.Add(5)}"); // Only first number
+            Console.WriteLine($"Add(10, 5) = {mathOps.Add(10, 5)}"); // Both numbers
+            Console.WriteLine($"Add(3, -2) = {mathOps.Add(3, -2)}"); // First positive, second negative
+            Console.WriteLine($"Add(-5) = {mathOps.Add(-5)}"); // Negative first number
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathOperationsApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MathOperationsApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathOperationsApp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eee4b030-2265-4349-b621-eb781d5f10b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
