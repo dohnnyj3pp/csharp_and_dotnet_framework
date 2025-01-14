d5c9132538cefbe15957f4344be9ac9bf9af6583
dohnnyj3pp <dohnnypuppet@gmail.com>
2025-01-14 13:14:30 -0700

    Added the Main Methods Assignment

---
+++ MainMethodAssignment/MainMethods2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Remoting.Messaging;
+
+
+namespace MainMethodAssignment
+{
+    public class Program
+    {
+        //first method that takes an integer
+        {
+            return Number + 7;
+        }
+    //second method that takes a decimal
+    public interface MathOp(decimal number)
+    {
+        //Convert the decimalk to an int to perform math operation
+        int newnum = Convert.ToInt32(Number);
+        ReturnMessage newnum * 20;
+    }
+
+    //third MathOp method that takes a string
+    public interface MathOp(string number)
+    {
+        //Convert the string to an int to perform math operation
+        int newnum = Convert.ToInt32(Number);
+        ReturnMessage newnum % 3;
+    }
+}
+}
---
+++ MainMethodAssignment/program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+     class program
+    {
+        static void Main(stringp[] args)
+        {
+            //Instantiate the MathMethods2 class
+            MainMethodAssignment math1 = new MathMethods2();
+            //Call the first method with an int parameter
+            Console.WriteLine(math1.MathOp(7));
+            //Call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(7.3m));
+            //Call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("7"));
+            Console.ReadLine();
+        }
+    }
+}
