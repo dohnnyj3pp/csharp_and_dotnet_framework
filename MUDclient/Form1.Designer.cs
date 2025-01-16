78df81efafff1f1b289c1b63dda9d1ffbe1c17c3
dohnnyj3pp <dohnnypuppet@gmail.com>
2025-01-16 12:04:56 -0700

    Began my first freelance project.  Developing a MUDclient to play a text base game.

---
+++ MUDclient/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MUDclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 418);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(800, 32);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(800, 412);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(698, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MUDclient/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MUDclient
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient _client;
+        private NetworkStream _stream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConnectToServer("3scapes.org", 3200);
+        }
+
+        private void ConnectToServer(string server, int port)
+        {
+            try
+            {
+                _client = new TcpClient(server, port);
+                _stream = _client.GetStream();
+                AppendMessage($"Connected to {server}:{port}");
+
+                Thread receiveThread = new Thread(ReceiveMessages);
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                AppendMessage($"Error: {ex.Message}");
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                try
+                {
+                    int bytesRead = _stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        AppendMessage(message);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AppendMessage($"Error: {ex.Message}");
+                    break;
+                }
+            }
+        }
+
+        private void AppendMessage(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(AppendMessage), message);
+            }
+            else
+            {
+                messageDisplayTextBox.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            string message = commandInputTextBox.Text;
+            SendMessage(message);
+            commandInputTextBox.Clear();
+        }
+
+        private void SendMessage(string message)
+        {
+            if (_stream != null)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message + "\n");
+                _stream.Write(data, 0, data.Length);
+            }
+        }
+    }
